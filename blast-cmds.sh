6ab8832b99e2611a71c2f57dbfd8e9a73292227c
Joel Robertson <joelwilliamrobertson@gmail.com>
2025-11-17 17:13:51 +1100

    updating blast commands

--- blast-cmds.sh
+++ blast-cmds.sh
@@ -7,4 +7,4 @@ aws s3 cp --no-sign-request s3://ncbi-blast-databases/2025-09-11-01-05-02/ blast
 #some number of threads is good (more the merrier, it will use them all full tilt, but there is some single threaded downtime between each batch that it runs, so don't pay for too many, diminishing returns ;) ) but the main thing is to ensure there is enough RAM to allow the database you are blasting to be cached.
 #You find this somewhat cryptically using the core_nt.nal (or *.nal for whatever db you use), which is just a json file with an entry "bytes-to-cache=". 
 ## In the case of core_nt, this is currently approx 235GB, so a high memory instance with 256GB RAM is perfect-- a bit of head room is required for other processes on top of the shared cache.
-blastn -num_threads 32 -query query.fasta -db /home/ec2-user/rna-seq-processing/blast_db/core_nt -out megaresults.txt -task megablast
+blastn -num_threads 32 -query query.fasta -db /home/ec2-user/rna-seq-processing/blast_db/core_nt -out results.txt -task megablast
