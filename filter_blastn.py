05cfd9bc990e34d190501f1f68c04df6d4668d7b
Joel Robertson <joelwilliamrobertson@gmail.com>
2025-11-18 02:26:58 +1100

    fixing script to filter blast results to include gaps and identities

--- filter_blastn.py
+++ filter_blastn.py
@@ -32,9 +32,9 @@ def select_hit(table_lines):
         line_counter += 1
         if "Sequences producing significant alignments:" in line:
             table_top = line_counter
-            if line_counter == table_top + 2:
-                ##gets highest rated general transcript
-                return line
+        if table_top>0 and line_counter == table_top + 2:
+            ##gets highest rated general transcript
+            return line
     return None
 
 # -------------------------------
@@ -44,7 +44,7 @@ out = open(output_file, "w", newline="")
 writer = csv.writer(out)
 writer.writerow([
     "QueryID","QueryLen","SeqID","Description",
-    "RNAType","SeqLen","Score","Evalue"
+    "RNAType","SeqLen","Score","Evalue","Identities","TotalIdentities","Gaps","TotalGaps"
 ])
 
 # -------------------------------
@@ -53,19 +53,21 @@ writer.writerow([
 inblock = False
 table_lines = []
 description_lines = []
-
+identity_cum = []
+gap_cum = []
 query_id = None
 query_len = None
 seq_id = None
 description = None
+identity = None
+gap = None
 rna_type = ""
 seq_len = None
 score = ""
 evalue = ""
 
-prev_line_blank = False
 collecting_description = False
-
+in_subject = False
 # -------------------------------
 # Parse BLAST file
 # -------------------------------
@@ -80,13 +82,17 @@ with open(input_file) as f:
             table_lines = []
             description_lines = []
             description = None
+            identity_cum = []
+            identity = None
+            gap_cum = []
+            gap = None
             rna_type = ""
             seq_len = None
             score = ""
             evalue = ""
             seq_id = None
-            prev_line_blank = False
             collecting_description = False
+            in_subject = False
 
             parts = line.split()
             query_id = parts[1]
@@ -99,7 +105,7 @@ with open(input_file) as f:
         # -------------------------------
         # Collect table lines until line beginning with >
         # -------------------------------
-        if not collecting_description:
+        if not in_subject and not collecting_description:
             if line.startswith("***** No hits found *****"):
             ## if at there were no hits found, we can stop here and write this as description 
                 writer.writerow([
@@ -110,6 +116,10 @@ with open(input_file) as f:
                     "",
                     "",
                     "",
+                    "",
+                    "",
+                    "",
+                    "",
                     ""
                 ])
                 # reset for next query
@@ -117,9 +127,13 @@ with open(input_file) as f:
                 table_lines = []
                 description_lines = []
                 description = None
+                identity_cum = []
+                identity = None
+                gap_cum = []
+                gap = None
                 seq_id = None
                 collecting_description = False
-                prev_line_blank = False
+                in_subject = False
                 continue
             if line.startswith(">"):
                 # if line starts with >, table finished a few lines back
@@ -131,16 +145,16 @@ with open(input_file) as f:
                         score = table_parts[-2]
                         evalue = table_parts[-1]
                     collecting_description = True
+                    in_subject = True
+                    #need to check if first > line matches selected
                     parts = line[1:].strip().split(None, 1)
                     if parts[0] == seq_id:
                         description_lines = [parts[1]] if len(parts) > 1 else []
                         continue
                     else:
                         continue
-                    #need to check if first > line matches selected
             else:
                 table_lines.append(line.strip())
-                prev_line_blank = False
             continue
 
         # -------------------------------
@@ -159,34 +173,75 @@ with open(input_file) as f:
                 stripped = line.strip()
                 if stripped.startswith("Length="):
                     description = " ".join(description_lines)
-                    lower_desc = description.lower()
+                    strip_desc = description.replace(",","")
+                    lower_desc = strip_desc.lower()
                     rna_type = ""
                     for t in rna_types_list:
                         if t in lower_desc:
                             rna_type = t.lower()
                             break
                     seq_len = stripped.split("=")[1]
+                    collecting_description = False
+                    continue
+                else:
+                    description_lines.append(stripped)
+        # -------------------------------
+        # Collect identity and gaps until next sequence/end
+        # -------------------------------
+                    
+        if in_subject and not collecting_description and seq_id:
+            #check if it is an identity line
+            stripped= line.strip()
+            if stripped.startswith("Identities"):
+                parts = stripped.split()
+                identity_cum.append(parts[2])
+                gap_cum.append(parts[6])
+                continue
+            #to end, check if at next sequence/at end of query
+            if line.startswith(">") or line.startswith("Effective"):
+                    #tally gap and identities
+                    identity_num = 0
+                    identity_den = 0
+                    for i in identity_cum:
+                        num,den = i.split("/")
+                        identity_num += int(num)
+                        identity_den += int(den)
+                    identity = f"{identity_num}/{identity_den}"
+                    gap_num = 0
+                    gap_den = 0
+                    for i in gap_cum:
+                        num,den = i.split("/")
+                        gap_num += int(num)
+                        gap_den += int(den)
+                    gap = f"{gap_num}/{gap_den}"
+                    #write out
                     writer.writerow([
                         query_id,
                         query_len,
                         seq_id,
-                        description if description else "",
+                        strip_desc if description else "",
                         rna_type,
                         seq_len if seq_len else "",
                         score,
-                        evalue
+                        evalue,
+                        identity_cum,
+                        identity,
+                        gap_cum,
+                        gap
+
                     ])
                     # reset for next query
                     inblock = False
                     table_lines = []
                     description_lines = []
                     description = None
+                    identity_cum = []
+                    identity = None
+                    gap_cum = []
+                    gap = None
                     seq_id = None
-                    collecting_description = False
-                    prev_line_blank = False
+                    in_subject = False
                     continue
-                else:
-                    description_lines.append(stripped)
             else:
                 continue
 
